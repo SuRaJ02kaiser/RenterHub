8e99350becd7156974fd60069f763e21efd22914
Suraj <Surajsinghnagrkoti2992003@gmail.com>
2025-06-02 01:12:44 +0530

    some changes

--- server/routes/property.route.js
+++ server/routes/property.route.js
@@ -2,10 +2,10 @@ const express = require("express");
 const authMiddleware = require("../middlewares/auth.middleware")
 const propertyRouter = express.Router();
 
-const {addProperty,deleteProperty,updateProperty,addTenantToProperty,removeTenantFromProperty,getPropertByName} = require("../controllers/property.controller")
+const {addProperty,deleteProperty,updateProperty,addTenantToProperty,removeTenantFromProperty,getPropertById} = require("../controllers/property.controller")
 
 propertyRouter.post("/addProperty",authMiddleware(["landlord"]),addProperty);
-propertyRouter.post("/getPropertyById/:id",authMiddleware(["landlord"]),getPropertById);
+propertyRouter.get("/getPropertyById/:id",authMiddleware(["landlord"]),getPropertById);
 propertyRouter.delete("/deleteProperty/:id",authMiddleware(["landlord"]),deleteProperty);
 propertyRouter.patch("/updateProperty/:id",authMiddleware(["landlord"]),updateProperty);
 propertyRouter.put("/addTenantToProperty/:tid",authMiddleware(["landlord"]),addTenantToProperty);
